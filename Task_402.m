4055c5f1b9e26149ce951d085c3d0cfb2e4edf26
charlotte342 <C.suwita@campus.lmu.de>
2025-03-06 16:45:34 +0100

    update 402

--- Task_402.m
+++ Task_402.m
@@ -41,6 +41,8 @@ for i = 1:np_Teilchen
     for j = i+1 : np_Teilchen
         if iD(j) == iD(i) % gleich wenn in einer Zelle
             PL(j).insertAfter(PL(i)); % dann: Verlinkung der Particle Lists
+%             r(j,:,i) = PL(i).Data - PL(j).Data; % Abstände Teilchen in einer Zelle
+%             r(i,:,j) = -r(j,:,i);
         end
     end
 
@@ -48,19 +50,90 @@ end
 
 % Zellen: gleicher Wert in Vektor iD
 n_cells = length(D); % Anzahl Zellen
-CellArray = cell(length(D), 1);
+CellArray_linear = cell(length(D),1);
 
 for i = 1:length(iD)
-    CellArray{iD(i),1} = [CellArray{iD(i),1}, PL(i)];
+    CellArray_linear{iD(i),1} = [CellArray_linear{iD(i),1}, PL(i)];
 end
+
 % Particle List - head einführen: jede Zelle mit M+1 Teilchen (erstes
-% Teilchen kopieren, dann ab i = 2
+% Teilchen kopieren
+% Zellen neu anordnen
+CellArray = cell(length(D));
 for i = 1:length(D)
-    CellArray{i,1} = [CellArray{i,1}(1,1),CellArray{i,1}];
+    CellArray{D(i,1), D(i,2)} = [CellArray_linear{i,1}(1,1),CellArray_linear{i,1}];
 end
 
 % Kraftberechnung - z. B. LJ_Kraft oder Coulomb ...
-F = LJ_Kraft(xyz, sigma, E, d);
+% Linked-Cell: Kraft Fi auf N(i) in Zelle ic aus kc Zellen (Nachbarzellen)
+% F = LJ_Kraft(xyz, sigma, E, d);
+
+
+n_cells_x = 3;
+n_cells_y = 3;
+% benachbarte Zellen finden:
+% alle relevanten Positionen für dieses Teilchen
+
+% [a, sigma, E, m, t, delta_t, n_steps, t_end] = Parameter('Parameter_302.txt');
+
+% tau = delta_t*1e3;
+% T_0 = 50;
+
+E = 1;
+sigma = 1; 
+
+%%
+tic
+
+F_total = zeros(np_Teilchen, 2);
+n_particle = zeros(n_cells_x, n_cells_y); % maximal alle Teilchen in einer Zelle
+for h = 1:np_Teilchen
+    for i= 1:n_cells_x % alle möglichen Zellen
+        for j = 1:n_cells_y
+            % alle relevanten Positionen für dieses Teilchen
+            n_particle(i,j) = size(CellArray{i,j}, 2); % Anzahl Teilchen pro Zelle {i,j}
+            n_gesamt = 0;
+            for k = -1:1 %  nur benachbarte Zellen, in 2D: 9 Zellen
+                for l = -1:1
+                    if i+k > 0 && j+l > 0 && i+k <= n_cells_x && j+l <= n_cells_y
+                        n_gesamt = n_gesamt + (n_particle(i+k,j+l)-1); % Gesamtzahl relevanter Teilchen für die Kraftberechnung
+%                         xyz = zeros(n_gesamt,2);
+                        for m = 2:n_particle(i+k,j+l) % ab 2 wegen particle head
+                            % Gesamtzahl an relevanten Teilchen
+                            coordinate(m-1,:,i+k,j+l) = CellArray{i+k,j+l}(1,m).Data; % relevante Koordinaten für Teilchen in Zelle ij --> Kraftberechnung
+%                             F(:,:,h) = LJ_Kraft(reshape(coordinate, size(coordinate,1) * size(coordinate, 3) * size(coordinate, 4), size(coordinate, 2)), 1, 1);
+                            %                         neu(:,:) = [coordinate(m,:, k+2, l+2)];
+                            xyz = reshape(coordinate, size(coordinate,1) * size(coordinate, 3) * size(coordinate, 4), size(coordinate, 2)); % neu angeordnet
+%                             xyz = coordinate;
+
+
+                        end
+                    end
+                end
+            end
+
+            n = size(xyz, 1);
+            F_neu = zeros(n,2);
+            r = zeros(n,2);
+            r_betrag = zeros(n,1,n);
+
+            for n = 1:n
+                for o = 1:n
+                    r(o,:) = xyz(n,:) - xyz(o,:); % Abstand n zu jedem Teilchen o
+                    F_neu(o,:) = 24*E/sum(r(o,:).^2, 2) * sigma/((sum(r(o,:).^2, 2))^3) * (1-2*sigma^6/(sum(r(o,:).^2,2)^3)) * r(o,:);
+                    %                                 r(:,:,n) = bsxfun(@minus, xyz(n,:), xyz);
+                    %                                 F_neu(:,:,n) = bsxfun(@times, (24*E./sum(r(:,:,n).^2, 2).*sigma^6./((sum(r(:,:,n).^2, 2)).^3).*(1-2*(sigma^6./(sum(r(:,:,n).^2,2).^3)))), r(:,:,n));
+                    F = F_neu;
+                    F(isnan(F_neu)) = 0;
+                end
+                F_h(n,:) = sum(F,1);
+            end
+            F_total(h,:) = F_h(n,:); % Gesamtkraft auf Teilchen h
+        end
+    end
+end
+
+toc
 
 % % Zeitintegration - z. B. Velocity-Verlet
 % [xyz_all, v, E_kin_all, E_pot_all, E_tot, T_all] = Zeitintegration(xyz, t, delta_t, F, n_steps, v, m, sigma, E, d, np_Teilchen, a, delta_t*1e3);
@@ -70,12 +143,12 @@ F = LJ_Kraft(xyz, sigma, E, d);
 for np = 1: Np
 ctrP = ctrP +1;
 % recursively until depth -> Np
-CellArray ( nc2 , nc1 ) . PL (1 ,1) . Next . Data . x (1 ,:) = X ( ctrP ,:) ;
-CellArray ( nc2 , nc1 ) . PL (1 ,1) = CellArray ( nc2 , nc1 ) . PL (1 ,1) . Next ;
+CellArray_linear ( nc2 , nc1 ) . PL (1 ,1) . Next . Data . x (1 ,:) = X ( ctrP ,:) ;
+CellArray_linear ( nc2 , nc1 ) . PL (1 ,1) = CellArray_linear ( nc2 , nc1 ) . PL (1 ,1) . Next ;
 end
 % reset ' pointer ' to initial :
 for np = 1: Np
-CellArray ( nc2 , nc1 ) . PL (1 ,1) = CellArray ( nc2 , nc1 ) . PL (1 ,1) . Prev ;
+CellArray_linear ( nc2 , nc1 ) . PL (1 ,1) = CellArray_linear ( nc2 , nc1 ) . PL (1 ,1) . Prev ;
 end
 
 
@@ -126,17 +199,17 @@ y = 0 : 1.5*sigma : 1.5*sigma*np_Teilchen;
 coordinates_0 = [X(:), Y(:)];
 end
 
-function F_total = LJ_Kraft(xyz, sigma, E, d)
+function F_total = LJ_Kraft(xyz, sigma, E) %d
 % Berechnung der Kräfte
 n = length(xyz);
-F_neu = zeros(n,3,n);
-r = zeros(n,3,n);
-F_total = zeros(n,3);
+F_neu = zeros(n,2,n);
+r = zeros(n,2,n);
+F_total = zeros(n,2);
 r_betrag = zeros(n,1,n);
 
 for i=1:n
     r(:,:,i) = bsxfun(@minus, xyz(i,:), xyz);
-    r(:,:,i) = r(:,:,i) - d.*round(r(:,:,i)./d);
+%     r(:,:,i) = r(:,:,i) - d.*round(r(:,:,i)./d);
     for j=i:n
         r_betrag(j,1,i) = norm(r(j,:,i));
         r_betrag(i,1,j) = r_betrag(j,1,i);
